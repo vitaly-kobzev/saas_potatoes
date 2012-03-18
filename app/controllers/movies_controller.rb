69426f004e3a66e9bb4295736748430a648ac644
Vitaly Kobzev <dzutte@yahoo.com>
2012-03-18 07:31:37 -0700

    init commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,48 @@
+class MoviesController < ApplicationController
+  
+  def initialize
+    super
+    @all_ratings = Movie.all_ratings
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @sort_by = params[:sort_by]
+    @ratings = params[:ratings]
+    @ratings_ary = @ratings ? @ratings.keys : @all_ratings
+    @movies = Movie.where(:rating => @ratings_ary).order(@sort_by)  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
